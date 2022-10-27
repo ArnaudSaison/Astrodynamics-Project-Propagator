70c2803d5d38bba84ed37cc8e685d800c27f285d
Antoine <antoinemontuzet@gmail.com>
2022-10-27 18:28:45 +0200

    testing errors divergence

--- propagator/matlab/Propagator01_Saison_Montuzet.m
+++ propagator/matlab/Propagator01_Saison_Montuzet.m
@@ -5,7 +5,7 @@ close all
 earth_radius = earthRadius/1000;
 earth_mu = 398600;
 
-% [r0, v0] = tle2eci(TLEISS);
+
 
 % initial condition of orbit parameters
 r_mag = earth_radius + 414;       % km
@@ -13,11 +13,14 @@ v_mag = sqrt(earth_mu/r_mag);     % km/s
 
 % initial position and velocity vector
 r0 = [r_mag,0,0];
-v0 = [0,v_mag-1,4];
+v0 = [0,v_mag,0];
+[r0, v0] = tle2eci(TLEISS.txt);
+% Time orbit
+t_orbit = (r_mag*2*pi)/v_mag;
 
 % Step
-n_step = 1000;
-t_end = 1*24*3600;
+n_step = 30*100;
+t_end = 100*t_orbit;
 tspan = linspace(0,t_end,n_step);
 
 % Array
@@ -33,8 +36,8 @@ toc
 % Plot earth and orbit
 plot_3D(earth_radius,stateout);
 
-xout = stateout(:,1);
-yout = stateout(:,2);
-zout = stateout(:,3);
-grdtrk([xout*1000,yout*1000,zout*1000]);
+% xout = stateout(:,1);
+% yout = stateout(:,2);
+% zout = stateout(:,3);
+% grdtrk([xout*1000,yout*1000,zout*1000]);
 
