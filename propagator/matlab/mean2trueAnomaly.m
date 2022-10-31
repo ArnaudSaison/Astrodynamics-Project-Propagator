19fc4be1abb9f85c35d69d3f7157cdd0efea28b8
Antoine <antoinemontuzet@gmail.com>
2022-10-31 16:07:26 +0100

    change equation for true anomaly

--- propagator/matlab/mean2trueAnomaly.m
+++ propagator/matlab/mean2trueAnomaly.m
@@ -4,11 +4,17 @@ function [theta] = mean2trueAnomaly(meanAno, ecc)
 
 % Eccentric anomaly
 eccAno =@(x) x - ecc * sin(x) - deg2rad(meanAno);
-E = fsolve(eccAno, 0)
+E = fsolve(eccAno, 0);
 
 % defining the relation between true anomaly and mean anomaly
-trueAno =@(x) 2 * atan(sqrt((1 - ecc)/(1 + ecc)) * tan(x / 2)) - E;
-theta = fsolve(trueAno, 0)
+% trueAno =@(x) 2 * atan(sqrt((1 - ecc)/(1 + ecc)) * tan(x / 2)) - E;
+% theta = fsolve(trueAno, 0)
+sinv = (sqrt(1-ecc.^2).*sin(E))./(1-ecc.*cos(E));   % Sine of true anomaly
+cosv = (cos(E)-ecc)./(1-ecc.*cos(E));               % Cosine of true anomaly
+theta = atan2(sinv,cosv);                           % True anomaly  [rad]
+if theta < 0
+    theta = 2*pi+theta;
+end
 
 theta = rad2deg(theta);
 
